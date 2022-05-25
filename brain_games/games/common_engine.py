97fc89eb12fa50ac5e10d8520b3c247603a24c9c
stconstantine <strkto@gmail.com>
2022-05-26 00:41:50 +0300

    splitted game in common engine to rolling rounds and managing game start/finish

--- brain_games/games/common_engine.py
+++ brain_games/games/common_engine.py
@@ -15,19 +15,26 @@ from brain_games.games import calc_game, even_game, \
 
 
 def game(game_name):
-    round_count = 0
-    user_winning = True
     username = interface.welcome_user(game_name)
+    user_won = _rounds_results(game_name)
 
+    if user_won:
+        interface.say_congrats(username)
+    else:
+        interface.say_try_again(username)
+
+
+def _rounds_results(game_name):
     games_catalog = {'calc_game': calc_game.calc_round,
-                          'even_game': even_game.even_round,
-                          'gcd_game': gcd_game.gcd_round,
-                          'prime_game': prime_game.prime_round,
-                          'progression_game': progression_game.progression_round
-                          }
+                     'even_game': even_game.even_round,
+                     'gcd_game': gcd_game.gcd_round,
+                     'prime_game': prime_game.prime_round,
+                     'progression_game': progression_game.progression_round
+                     }
 
     get_round_results = games_catalog[game_name]
-
+    round_count = 0
+    user_winning = True
     while round_count < WINS_COUNT_REQUIRED and user_winning:
         round_count += 1
         user_answer, correct_answer = get_round_results()
@@ -36,8 +43,4 @@ def game(game_name):
             interface.say_correct()
         else:
             interface.say_wrong_correct(user_answer, correct_answer)
-
-    if user_winning:
-        interface.say_congrats(username)
-    else:
-        interface.say_try_again(username)
+    return user_winning
